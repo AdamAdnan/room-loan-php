15c4c214a06094b49bc4d5c646848230bf94ac74
Adam Adnan <echo.adnan100701@gmail.com>
2023-12-21 23:55:28 +0700

    firts commit

---
+++ app/models/User_model.php
@@ -0,0 +1,9 @@
+<?php
+
+Class User_model {
+    private $nama = 'Adam Adnan';
+    
+    public function getUser(){
+        return $this->nama;
+    }
+}
---
+++ app/views/home/index.php
@@ -0,0 +1,17 @@
+<div class="container">
+
+    <div class="jumbotron">
+        <h1>Selamat Datang di Website Saya!</h1>
+        <p class="lead">
+            Halo, nama saya <?= $data['nama']; ?>
+        </p>
+        <hr class="my-4">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, recusandae culpa dolor soluta ab repellat provident eaque obcaecati inventore molestiae, deleniti porro error numquam quae dignissimos asperiores odio vitae ullam!
+        </p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">
+            Learn more
+        </a>
+    </div>
+
+</div>
